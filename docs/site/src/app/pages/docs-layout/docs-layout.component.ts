b0b97d00c859430ec47591d57841b2fbbd42cecc
htuna07 <h.tuna96@gmail.com>
2021-02-22 18:50:19 +0300

    remove console.log

--- docs/site/src/app/pages/docs-layout/docs-layout.component.ts
+++ docs/site/src/app/pages/docs-layout/docs-layout.component.ts
@@ -2,7 +2,7 @@ import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {Component, OnInit, OnDestroy} from "@angular/core";
 import {Router} from "@angular/router";
 import {Observable, Subject} from "rxjs";
-import {map, startWith, takeUntil, tap} from "rxjs/operators";
+import {map, startWith, takeUntil} from "rxjs/operators";
 import {DocService} from "../../services/doc.service";
 
 @Component({
@@ -11,7 +11,6 @@ import {DocService} from "../../services/doc.service";
   styleUrls: ["./docs-layout.component.scss"]
 })
 export class DocsLayoutComponent implements OnInit, OnDestroy {
-  // $apiDocs: Observable<any>;
   $contentDocs: Observable<any>;
   headingsInContent: {title: string; fragment: string}[];
   activeDocument: string;
@@ -29,8 +28,7 @@ export class DocsLayoutComponent implements OnInit, OnDestroy {
     private breakpointObserver: BreakpointObserver,
     private route: Router
   ) {
-    // this.$apiDocs = docs.getApiDocs();
-    this.$contentDocs = docs.getContentDocs().pipe(tap(console.log));
+    this.$contentDocs = docs.getContentDocs();
 
     docs.documentChanged.pipe(takeUntil(this.onDestroy)).subscribe(data => {
       this.headingsInContent = [];
